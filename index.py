c453d293f3e1b04e7b14372b1cfeee339f79b49d
Stephen Korankye <53407122+stephenkorankye@users.noreply.github.com>
2022-11-10 02:23:15 +0000

    Initial Commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup 
+import requests , openpyxl 
+
+
+new_dict = dict("Dave" = '001' , Ava= '002' , Joe= '003') 
+
+for i in new_dict : 
+    print ( new_dict[i] )
+
+
+
+
