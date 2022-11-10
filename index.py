7dfa2b2087c3ca62cb24f3d67de2eb116d152b24
Stephen Korankye <53407122+stephenkorankye@users.noreply.github.com>
2022-11-10 04:47:40 +0000

    Making Progress

--- index.py
+++ index.py
@@ -13,17 +13,27 @@ def single_course(url) :
     source.raise_for_status() 
 
     soup = BeautifulSoup(source.text , "html.parser")
-    main = soup.find("main").find("div" , class_="rc-BannerInstructorInfo")
-    main = main.find("a").get_text(strip=True)
+    main = soup.find("main") 
 
-    print ( main ) 
+    instructor = main.find("div" , class_="rc-BannerInstructorInfo")
+    instructor = instructor.find("a").get_text(strip=True).split("Â")[0]
+
+    enrolled = soup.find("div" , class_="rc-ProductMetrics").find("strong").find("span").text
+
+    course_name = soup.find("h1" , class_="banner-title m-b-0").text
+
+    description = soup.find("div" , class_="description").find("div" , class_="description").text
+    
+    ratings = soup.find("div" , class_="_wmgtrl9 color-white ratings-count-expertise-style").text.split(" ")[0]
+    print ( ratings ) 
+
 
 
     
 
-# url = "https://www.coursera.org/specializations/statistics"
+url = "https://www.coursera.org/specializations/statistics"
 # url = "https://www.coursera.org/learn/marketing-analytics-foundation"
-url = "https://www.coursera.org/professional-certificates/ibm-data-science"
+# url = "https://www.coursera.org/professional-certificates/ibm-data-science"
 
 single_course(url) 
 
